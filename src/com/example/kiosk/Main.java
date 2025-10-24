c96185ac970df5fdc749d212d50cdfb0274229dc
DESKTOP-HIO8OC0\User <uhk561@naver.com>
2025-10-24 21:57:49 +0900

    lv 2 선택된 메뉴,가격,설명 출력하기 제외 구현완료

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,28 +1,34 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main { // 시작지점이 되는 클래스
 
     public static void main(String[] args) {
 
+        // List를 선언하고 객체생성과 동시에 이름, 가격, 설명 세팅
+       List<MenuItem> menuItems = new ArrayList<>();
+       menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+       menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+       menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+       menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        // Scanner 선언
         Scanner sc  = new Scanner(System.in);
 
 
-        // 메뉴를 담은 이차원배열
-        String[][] Menu = {{"ShackBurger", "6.9", "토마토, 양상추, 쉑소스가 토핑된 치즈버거"}
-                ,{"SmokeShack", "8.9", "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"}
-                ,{"Cheeseburger", "6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"}
-                ,{"Hamburger", "5.4", "비프패티를 기반으로 야채가 들어간 기본버거"},
-        };
 
+
+       // main 함수에서 MenuItem 클래스를 활용하여 햄버거 메뉴를 출력합니다.
         System.out.println("[ SHAKESHACK MENU ]");
-        for (int i = 0; i < Menu.length; i++) {
-                System.out.println(i + 1 + ". " + Menu[i][0] + " | " + Menu[i][1] + " | " + Menu[i][2]);
+        for (int i = 0; i < menuItems.size(); i++) {
+                System.out.print(i + 1 + ". ");
+                menuItems.get(i).printMenu();
         }
         System.out.println("0. 종료");
         System.out.println("알맞은 번호를 입력하세요.");
-
 
 
 
@@ -36,14 +42,16 @@ public class Main { // 시작지점이 되는 클래스
             if(answer == 0) {
                 System.out.println("종료되었습니다.");
                 break;
-            } else if (1 <= answer && answer<= Menu.length){   // 입력값이 1부터 Menu의 인덱스 길이까지만 허용 (범위검사
-                System.out.println(Menu[answer - 1][0] + "를 선택하셨습니다");
+            } else if (1 <= answer && answer<= menuItems.size()){   // 리스트 사이즈까지 범위 허용 (범위검사
+                System.out.println(menuItems.get(answer - 1) + "를 선택하셨습니다");
                 break;
             } else {
                 System.out.println("올바른 입력이 아닙니다.");
             }
 
         }
+        System.out.println("종료되었습니다");
+        sc.close();
     }
 }
 
--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,5 +1,30 @@
 package com.example.kiosk;
 
-public class MenuItem { //세부 메뉴 속성을 가지는 클래스 
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuItem { //세부 메뉴 속성을 가지는 클래스
+
+    // 속성
+    private String name;
+    private double price;
+    private String info;
+
+
+    // 생성자
+    public MenuItem(String name, double price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    // 기능
+    public void printMenu() { // 메뉴출력
+        System.out.println(name + " | " + "W " + price + " | " + info);
+    }
+
+    // 게터
+    public String getName() {
+        return this.name;
+    }
 }
