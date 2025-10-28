af39a9b7d403e8fbddab14933209ab1278a582e3
DESKTOP-HIO8OC0\User <uhk561@naver.com>
2025-10-28 12:00:57 +0900

    lv4 카테고리 및 하위 메뉴 출력 및 선택구현(뒤로가기 제외)

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -35,31 +35,42 @@ public class Kiosk { // 프로그램 순서 및 흐름 제어 담당
                 System.out.println("올바른 입력이 아닙니다.");
                 continue;
             }
-            int choiceMenu= sc.nextInt();
-            if(choiceMenu == 0) {
+            int choicecategory= sc.nextInt();
+            if(choicecategory == 0) {
                 System.out.println("종료되었습니다.");
                 break;
-            } else if (1 <= choiceMenu && choiceMenu <= categories.size()){   // 리스트 사이즈까지 범위 허용 (범위검사
-                System.out.println(categories.get(choiceMenu - 1).getName() + "를(을) 선택하셨습니다");
+            } else if (1 <= choicecategory && choicecategory <= categories.size()){   // 리스트 사이즈까지 범위 허용 (범위검사
+                System.out.println(categories.get(choicecategory - 1).getName() + "를(을) 선택하셨습니다");
             } else {
                 System.out.println("올바른 입력이 아닙니다.");
             }
 
-            Menu choiceItems = categories.get(choiceMenu - 1);
-            List<MenuItem> items = choiceItems.getMenuItems();
+            Menu choiceMenu = categories.get(choicecategory - 1); // 선택된 카테고리 안의 메뉴들 가져오기
+            List<MenuItem> items = choiceMenu.getMenuItems();
+
             for (int i = 0; i < items.size(); i++) {
-                System.out.println(i + 1 + ". ");
+                System.out.print(i + 1 + ". ");
                 items.get(i).printMenu();
+
             }
 
             System.out.println("0. 뒤로가기");
+
             if(!sc.hasNextInt()) {
                 sc.next();
                 System.out.println("올바른 입력이 아닙니다.");
                 continue;
             }
-
-
+            int choiceItem = sc.nextInt();
+            if (choiceItem == 0) {
+                continue;
+            } else if (1 <= choiceItem && choiceItem <= items.size()){   // 리스트 사이즈까지 범위 허용 (범위검사
+                MenuItem choiceOrder= items.get(choiceItem - 1); // 선택한 메뉴 가져오기 ex(A버거
+                System.out.println(items.get(choiceItem - 1).printChoicMenu() + "를(을) 선택하셨습니다");
+                break;
+            } else {
+               System.out.println("올바른 입력이 아닙니다.");
+            }
 
         }
 
